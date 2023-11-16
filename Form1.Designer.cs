89f9f283169a4c37c4ffa8deb3de922c49a66c3b
Noreh AD <akinoreh@gmail.com>
2023-11-16 22:50:13 +0300

    extract out treeview population

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Program Data");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Program Files");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Program Files (x86)");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Users");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Windows");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Local Disk (C:)", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3,
-            treeNode4,
-            treeNode5});
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("This PC", new System.Windows.Forms.TreeNode[] {
-            treeNode6});
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
@@ -49,29 +36,6 @@
             this.treeView1.Location = new System.Drawing.Point(9, 9);
             this.treeView1.Margin = new System.Windows.Forms.Padding(0);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Node2";
-            treeNode1.Tag = "C:\\ProgramData";
-            treeNode1.Text = "Program Data";
-            treeNode2.Name = "Node3";
-            treeNode2.Tag = "C:\\Program Files";
-            treeNode2.Text = "Program Files";
-            treeNode3.Name = "Node4";
-            treeNode3.Tag = "C:\\Program Files (x86)";
-            treeNode3.Text = "Program Files (x86)";
-            treeNode4.Name = "Node5";
-            treeNode4.Tag = "C:\\Users";
-            treeNode4.Text = "Users";
-            treeNode5.Name = "Node6";
-            treeNode5.Tag = "C:\\Windows";
-            treeNode5.Text = "Windows";
-            treeNode6.Name = "Node1";
-            treeNode6.Tag = "C:\\";
-            treeNode6.Text = "Local Disk (C:)";
-            treeNode7.Name = "Node0";
-            treeNode7.Tag = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
-            treeNode7.Text = "This PC";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode7});
             this.treeView1.Size = new System.Drawing.Size(200, 250);
             this.treeView1.TabIndex = 10;
             this.treeView1.TabStop = false;
@@ -89,7 +53,6 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
-
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,43 @@ namespace this_pc
         public Form1()
         {
             InitializeComponent();
+            PopulateTreeView(this.treeView1);
+        }
+
+        private void PopulateTreeView(TreeView treeView)
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("This PC");
+            treeNode1.Tag = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Local Disk (C:)");
+            treeNode2.Tag = "C:\\";
+
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Program Data");
+            treeNode3.Tag = "C:\\ProgramData";
+
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Program Files");
+            treeNode4.Tag = "C:\\Program Files";
+
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Program Files (x86)");
+            treeNode5.Tag = "C:\\Program Files (x86)";
+
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Users");
+            treeNode6.Tag = "C:\\Users";
+
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Windows");
+            treeNode7.Tag = "C:\\Windows";
+
+            treeNode1.Nodes.Add(treeNode2);
+
+            treeNode2.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+                treeNode3,
+                treeNode4,
+                treeNode5,
+                treeNode6,
+                treeNode7,
+            });
+
+            treeView.Nodes.Add(treeNode1);
         }
 
         private void Form1_Load(object sender, EventArgs e)
