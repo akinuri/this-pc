b01fb72011cc91435aab5076dd8c52981a0ef35b
Noreh AD <akinoreh@gmail.com>
2023-11-17 23:28:22 +0300

    populate from locations text

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Timer = System.Windows.Forms.Timer;
 using TreeNode = System.Windows.Forms.TreeNode;
 
@@ -8,7 +9,8 @@ namespace this_pc
     {
         public string Text { get; set; }
         public string Tag { get; set; }
-        public List<LocationNodeRecord>? Children { get; set; }
+        public List<LocationNodeRecord> Children = new List<LocationNodeRecord>();
+        public int Level = 0;
     }
 
     public partial class Form1 : Form
@@ -21,49 +23,61 @@ namespace this_pc
 
         private void PopulateTreeView(TreeView treeView)
         {
-            List<LocationNodeRecord> locations = new List<LocationNodeRecord>
+            string locationsText = @"
+            This PC, ::{20D04FE0-3AEA-1069-A2D8-08002B30309D}
+                Local Disk (C:), C:\
+                    Program Data, C:\ProgramData
+                    Program Files, C:\Program Files,
+                    Program Files (x86), C:\Program Files (x86)
+                    Users, C:\Users
+                    Windows, C:\Windows
+            ";
+            locationsText = RemoveUnnecessaryIndentation(locationsText);
+
+            string[] lines = Regex.Split(locationsText, @"\r?\n");
+
+            List<LocationNodeRecord> locations = new List<LocationNodeRecord> { };
+
+            List<LocationNodeRecord> parentNodes = new List<LocationNodeRecord>();
+            LocationNodeRecord? prevNode = null;
+
+            foreach (var line in lines)
             {
-                new LocationNodeRecord
+                var indentMatch = Regex.Match(line, @"^ *");
+                string indent = indentMatch.Success ? indentMatch.Value : "";
+                var indentLevelMatches = Regex.Matches(indent, @" {4}");
+                int indentLevel = indentLevelMatches.Count;
+                string[] parts = Regex.Split(line, @", *");
+                string text = parts[0].Trim();
+                string tag = parts[1].Trim();
+                LocationNodeRecord locNodeRec = new LocationNodeRecord { Text = text, Tag = tag, Level = indentLevel };
+                if (prevNode == null)
                 {
-                    Text = "This PC",
-                    Tag = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}",
-                    Children = new List<LocationNodeRecord>
+                    locations.Add(locNodeRec);
+                }
+                else
+                {
+                    if (locNodeRec.Level > prevNode.Level)
                     {
-                        new LocationNodeRecord
-                        {
-                            Text = "Local Disk (C:)",
-                            Tag = "C:\\",
-                            Children = new List<LocationNodeRecord> {
-                                new LocationNodeRecord
-                                {
-                                    Text = "Program Data",
-                                    Tag = "C:\\ProgramData",
-                                },
-                                new LocationNodeRecord
-                                {
-                                    Text = "Program Files",
-                                    Tag = "C:\\Program Files",
-                                },
-                                new LocationNodeRecord
-                                {
-                                    Text = "Program Files (x86)",
-                                    Tag = "C:\\Program Files (x86)",
-                                },
-                                new LocationNodeRecord
-                                {
-                                    Text = "Users",
-                                    Tag = "C:\\Users",
-                                },
-                                new LocationNodeRecord
-                                {
-                                    Text = "Windows",
-                                    Tag = "C:\\Windows",
-                                },
-                            },
-                        },
-                    },
-                },
-            };
+                        parentNodes.Add(prevNode);
+                    }
+                    else if (locNodeRec.Level < prevNode.Level)
+                    {
+                        int levelDiff = Math.Abs(locNodeRec.Level - prevNode.Level);
+                        parentNodes.RemoveRange(parentNodes.Count - levelDiff, levelDiff);
+                    }
+                    if (parentNodes.Count == 0)
+                    {
+                        locations.Add(locNodeRec);
+                    }
+                    else
+                    {
+                        var lastParent = parentNodes[parentNodes.Count - 1];
+                        lastParent.Children.Add(locNodeRec);
+                    }
+                }
+                prevNode = locNodeRec;
+            }
 
             foreach (LocationNodeRecord location in locations)
             {
@@ -77,7 +91,28 @@ namespace this_pc
                     }
                 }
             }
+        }
 
+        static string RemoveUnnecessaryIndentation(string indentedString)
+        {
+            var match = Regex.Match(indentedString, @"^[ ]+", RegexOptions.Multiline);
+            int commonIndentationLength = match.Success ? match.Value.Length : 0;
+            string[] lines = Regex.Split(indentedString, @"\r?\n");
+            var processedLines = new List<string>();
+            foreach (var line in lines)
+            {
+                if (line.Trim().Length > 0)
+                {
+                    var _line = line;
+                    if (_line.Length >= commonIndentationLength)
+                    {
+                        _line = line.Substring(commonIndentationLength);
+                    }
+                    _line = _line.TrimEnd();
+                    processedLines.Add(_line);
+                }
+            }
+            return string.Join(Environment.NewLine, processedLines);
         }
 
         private void ProcessChildLocations(LocationNodeRecord location, TreeNode parentNode)
