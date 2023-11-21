e07545958057dfb0f0f601ed00f4b5e1f8d75832
Noreh AD <akinoreh@gmail.com>
2023-11-22 01:10:37 +0300

    summary: GetSystemInfo()

--- Summary.cs
+++ Summary.cs
@@ -4,12 +4,10 @@ using Microsoft.Management.Infrastructure.Options;
 public static class Summary
 {
 
-    public static void PopulateListView(ListView listView)
+    public static Dictionary<string, string> GetSystemInfo(string[]? keys = null)
     {
-        string? manufacturer = null;
-        string? model = null;
-        string? sku = null;
-
+        keys ??= new string[0];
+        Dictionary<string, string> info = new Dictionary<string, string>();
         try
         {
             CimSession cimSession = CimSession.Create(null);
@@ -23,9 +21,19 @@ public static class Summary
             }
             if (result != null)
             {
-                manufacturer = result.CimInstanceProperties["Manufacturer"].Value.ToString();
-                model = result.CimInstanceProperties["Model"].Value.ToString();
-                sku = result.CimInstanceProperties["SystemSKUNumber"].Value.ToString();
+                foreach (var item in result.CimInstanceProperties)
+                {
+                    string value = item.Value?.ToString() ?? "";
+                    var stringArray = item.Value as string[];
+                    if (stringArray != null)
+                    {
+                        value = string.Join(", ", stringArray);
+                    }
+                    if (keys.Count() == 0 || keys.Contains(item.Name))
+                    {
+                        info.Add(item.Name, value);
+                    }
+                }
             }
             cimSession.Dispose();
         }
@@ -33,15 +41,21 @@ public static class Summary
         {
             //Logger.error(ex.Message);
         }
+        return info;
+    }
+
+    public static void PopulateListView(ListView listView)
+    {
+        var summary = GetSystemInfo(new string[3] { "Manufacturer", "Model", "SystemSKUNumber" });
 
         ListViewGroup listViewGroup1 = new ListViewGroup("Device", HorizontalAlignment.Left);
         listViewGroup1.Header = "Device";
         listViewGroup1.Name = "listViewGroup1";
         listView.Groups.AddRange(new ListViewGroup[] { listViewGroup1 });
 
-        ListViewItem listViewItem1 = new ListViewItem(new string[] {"Manufacturer", manufacturer ?? ""}, -1);
-        ListViewItem listViewItem2 = new ListViewItem(new string[] {"Model", model ?? "" }, -1);
-        ListViewItem listViewItem3 = new ListViewItem(new string[] {"SKU", sku ?? "" }, -1);
+        ListViewItem listViewItem1 = new ListViewItem(new string[] {"Manufacturer", summary["Manufacturer"] ?? ""}, -1);
+        ListViewItem listViewItem2 = new ListViewItem(new string[] {"Model", summary["Model"] ?? "" }, -1);
+        ListViewItem listViewItem3 = new ListViewItem(new string[] {"SKU", summary["SystemSKUNumber"] ?? "" }, -1);
 
         listViewItem1.Group = listViewGroup1;
         listViewItem2.Group = listViewGroup1;
