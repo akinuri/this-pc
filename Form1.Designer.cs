d4fb503d679cce461a46c260667389a65be61ede
Noreh AD <akinoreh@gmail.com>
2023-11-22 17:44:22 +0300

    layout changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace this_pc
+namespace this_pc
 {
     partial class Form1
     {
@@ -44,7 +44,7 @@
             // LocationsTreeView
             // 
             this.LocationsTreeView.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.LocationsTreeView.Location = new System.Drawing.Point(10, 31);
+            this.LocationsTreeView.Location = new System.Drawing.Point(9, 31);
             this.LocationsTreeView.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
             this.LocationsTreeView.Name = "LocationsTreeView";
             this.LocationsTreeView.Size = new System.Drawing.Size(240, 520);
@@ -57,7 +57,7 @@
             // 
             this.LocationsLabel.AutoSize = true;
             this.LocationsLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.LocationsLabel.Location = new System.Drawing.Point(10, 9);
+            this.LocationsLabel.Location = new System.Drawing.Point(9, 9);
             this.LocationsLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 5);
             this.LocationsLabel.Name = "LocationsLabel";
             this.LocationsLabel.Size = new System.Drawing.Size(65, 17);
@@ -73,10 +73,10 @@
             this.columnHeader2});
             this.SummaryListView.FullRowSelect = true;
             this.SummaryListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.SummaryListView.Location = new System.Drawing.Point(10, 31);
+            this.SummaryListView.Location = new System.Drawing.Point(9, 31);
             this.SummaryListView.Margin = new System.Windows.Forms.Padding(0);
             this.SummaryListView.Name = "SummaryListView";
-            this.SummaryListView.Size = new System.Drawing.Size(309, 192);
+            this.SummaryListView.Size = new System.Drawing.Size(304, 200);
             this.SummaryListView.TabIndex = 13;
             this.SummaryListView.UseCompatibleStateImageBehavior = false;
             this.SummaryListView.View = System.Windows.Forms.View.Details;
@@ -95,7 +95,7 @@
             // 
             this.SummaryLabel.AutoSize = true;
             this.SummaryLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.SummaryLabel.Location = new System.Drawing.Point(10, 9);
+            this.SummaryLabel.Location = new System.Drawing.Point(9, 10);
             this.SummaryLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 5);
             this.SummaryLabel.Name = "SummaryLabel";
             this.SummaryLabel.Size = new System.Drawing.Size(67, 17);
@@ -115,23 +115,26 @@
             // 
             this.splitContainer1.Panel1.Controls.Add(this.SummaryListView);
             this.splitContainer1.Panel1.Controls.Add(this.SummaryLabel);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(10);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.LocationsTreeView);
             this.splitContainer1.Panel2.Controls.Add(this.LocationsLabel);
-            this.splitContainer1.Size = new System.Drawing.Size(596, 566);
-            this.splitContainer1.SplitterDistance = 329;
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.splitContainer1.Size = new System.Drawing.Size(588, 566);
+            this.splitContainer1.SplitterDistance = 322;
             this.splitContainer1.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 569);
+            this.ClientSize = new System.Drawing.Size(588, 565);
             this.Controls.Add(this.splitContainer1);
-            this.MaximumSize = new System.Drawing.Size(800, 608);
-            this.MinimumSize = new System.Drawing.Size(612, 608);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 604);
+            this.MinimumSize = new System.Drawing.Size(604, 604);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "This PC";
