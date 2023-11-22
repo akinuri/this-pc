9e8c16073e4b3e4d8f66585d30c1849ae6cb657b
Noreh AD <akinoreh@gmail.com>
2023-11-22 17:47:51 +0300

    read summary from text file

--- Summary.cs
+++ Summary.cs
@@ -1,8 +1,50 @@
 ﻿using Microsoft.Management.Infrastructure;
 using Microsoft.Management.Infrastructure.Options;
+using System.Text.RegularExpressions;
 
 public static class Summary
 {
+    public static string summaryFilePath = "summary.txt";
+
+    private static string[] readSummaryFile(Func<string, string>? callback = null)
+    {
+        string[] lines = new string[0];
+        if (!File.Exists(summaryFilePath))
+        {
+            return lines;
+        }
+        string text = File.ReadAllText(summaryFilePath);
+        text = callback?.Invoke(text) ?? text;
+        lines = text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        return lines;
+    }
+
+    private static string ReplaceVarPlaceHolders(string text)
+    {
+        Dictionary<string, string> varPropMap = new Dictionary<string, string>
+        {
+            {"%Manufacturer%",  "Manufacturer"},
+            {"%Model%",         "Model"},
+            {"%SKU%",           "SystemSKUNumber"},
+            {"%Name%",          "Name"},
+            {"%User%",          "PrimaryOwnerName"},
+        };
+        List<string> usedProps = new List<string>();
+        foreach (var item in varPropMap)
+        {
+            if (text.Contains(item.Key))
+            {
+                usedProps.Add(item.Value);
+            }
+        }
+        Dictionary<string, string> info = GetSystemInfo(usedProps.ToArray());
+        foreach (KeyValuePair<string, string> pair in info)
+        {
+            string var = varPropMap.FirstOrDefault(x => x.Value == pair.Key).Key;
+            text = text.Replace(var, pair.Value);
+        }
+        return text;
+    }
 
     public static Dictionary<string, string> GetSystemInfo(string[]? keys = null)
     {
@@ -44,28 +86,74 @@ public static class Summary
         return info;
     }
 
-    public static void PopulateListView(ListView listView)
+    private static List<SummaryNode> buildSummaryList(string[]? lines = null)
     {
-        var summary = GetSystemInfo(new string[3] { "Manufacturer", "Model", "SystemSKUNumber" });
-
-        ListViewGroup listViewGroup1 = new ListViewGroup("Device", HorizontalAlignment.Left);
-        listViewGroup1.Header = "Device";
-        listViewGroup1.Name = "listViewGroup1";
-        listView.Groups.AddRange(new ListViewGroup[] { listViewGroup1 });
-
-        ListViewItem listViewItem1 = new ListViewItem(new string[] {"Manufacturer", summary["Manufacturer"] ?? ""}, -1);
-        ListViewItem listViewItem2 = new ListViewItem(new string[] {"Model", summary["Model"] ?? "" }, -1);
-        ListViewItem listViewItem3 = new ListViewItem(new string[] {"SKU", summary["SystemSKUNumber"] ?? "" }, -1);
-
-        listViewItem1.Group = listViewGroup1;
-        listViewItem2.Group = listViewGroup1;
-        listViewItem3.Group = listViewGroup1;
-
-        listView.Items.AddRange(new ListViewItem[] {
-            listViewItem1,
-            listViewItem2,
-            listViewItem3,
-        });
+        lines ??= new string[0];
+        List<SummaryNode> summary = new List<SummaryNode>();
+        SummaryNode? lastParentNode = null;
+        foreach (var line in lines)
+        {
+            int indentLevel = Utilities.GetIndentLevel(line);
+            string[] parts = Regex.Split(line, @", *");
+            if (parts.Length == 0) { continue; }
+            SummaryNode node = new()
+            {
+                Name = parts[0].Trim(),
+                Value = parts.Length > 1 ? parts[1]?.Trim() : null,
+                Level = Utilities.GetIndentLevel(line),
+            };
+            if (node.Level == 0)
+            {
+                summary.Add(node);
+                lastParentNode = node;
+            }
+            else
+            {
+                if (lastParentNode != null)
+                {
+                    lastParentNode.Children.Add(node);
+                }
+                else
+                {
+                    summary.Add(node);
+                }
+            }
+        }
+        return summary;
     }
 
+    public static List<SummaryNode> GetSummary()
+    {
+        string[] lines = readSummaryFile(text => ReplaceVarPlaceHolders(text));
+        var summary = buildSummaryList(lines);
+        return summary;
+    }
+
+    public static void PopulateListView(ListView listView)
+    {
+        List<SummaryNode> summary = GetSummary();
+        foreach (SummaryNode node in summary)
+        {
+            if (node.Level == 0)
+            {
+                ListViewGroup group = new ListViewGroup(node.Name, HorizontalAlignment.Left);
+                listView.Groups.AddRange(new ListViewGroup[] { group });
+                foreach (SummaryNode childNode in node.Children)
+                {
+                    ListViewItem item = new ListViewItem(new string[] {childNode.Name, childNode.Value ?? ""});
+                    item.Group = group;
+                    listView.Items.Add(item);
+                }
+            }
+        }
+    }
+
+}
+
+public class SummaryNode
+{
+    public string Name { get; set; }
+    public string? Value { get; set; }
+    public List<SummaryNode> Children = new List<SummaryNode>();
+    public int Level = 0;
 }
