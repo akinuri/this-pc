4dbe774065f88a2470197d10549bdeada14e4fd4
Noreh AD <akinoreh@gmail.com>
2023-11-25 18:56:43 +0300

    auto-optimizations from vs update?

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,127 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LocationsTreeView = new System.Windows.Forms.TreeView();
-            this.LocationsLabel = new System.Windows.Forms.Label();
-            this.SummaryListView = new System.Windows.Forms.ListView();
-            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-            this.SummaryLabel = new System.Windows.Forms.Label();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            this.SuspendLayout();
+            LocationsTreeView = new TreeView();
+            LocationsLabel = new Label();
+            SummaryListView = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            SummaryLabel = new Label();
+            splitContainer1 = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
             // 
             // LocationsTreeView
             // 
-            this.LocationsTreeView.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.LocationsTreeView.Location = new System.Drawing.Point(9, 31);
-            this.LocationsTreeView.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.LocationsTreeView.Name = "LocationsTreeView";
-            this.LocationsTreeView.Size = new System.Drawing.Size(230, 520);
-            this.LocationsTreeView.TabIndex = 10;
-            this.LocationsTreeView.TabStop = false;
-            this.LocationsTreeView.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCollapse);
-            this.LocationsTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            LocationsTreeView.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            LocationsTreeView.Location = new Point(9, 31);
+            LocationsTreeView.Margin = new Padding(10, 0, 0, 0);
+            LocationsTreeView.Name = "LocationsTreeView";
+            LocationsTreeView.Size = new Size(230, 520);
+            LocationsTreeView.TabIndex = 10;
+            LocationsTreeView.TabStop = false;
+            LocationsTreeView.BeforeCollapse += treeView1_BeforeCollapse;
+            LocationsTreeView.NodeMouseDoubleClick += treeView1_NodeMouseDoubleClick;
             // 
             // LocationsLabel
             // 
-            this.LocationsLabel.AutoSize = true;
-            this.LocationsLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.LocationsLabel.Location = new System.Drawing.Point(9, 9);
-            this.LocationsLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 5);
-            this.LocationsLabel.Name = "LocationsLabel";
-            this.LocationsLabel.Size = new System.Drawing.Size(65, 17);
-            this.LocationsLabel.TabIndex = 11;
-            this.LocationsLabel.Text = "Locations";
+            LocationsLabel.AutoSize = true;
+            LocationsLabel.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            LocationsLabel.Location = new Point(9, 9);
+            LocationsLabel.Margin = new Padding(0, 0, 0, 5);
+            LocationsLabel.Name = "LocationsLabel";
+            LocationsLabel.Size = new Size(65, 17);
+            LocationsLabel.TabIndex = 11;
+            LocationsLabel.Text = "Locations";
             // 
             // SummaryListView
             // 
-            this.SummaryListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.SummaryListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2});
-            this.SummaryListView.FullRowSelect = true;
-            this.SummaryListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.SummaryListView.Location = new System.Drawing.Point(9, 31);
-            this.SummaryListView.Margin = new System.Windows.Forms.Padding(0);
-            this.SummaryListView.Name = "SummaryListView";
-            this.SummaryListView.Size = new System.Drawing.Size(204, 400);
-            this.SummaryListView.TabIndex = 13;
-            this.SummaryListView.UseCompatibleStateImageBehavior = false;
-            this.SummaryListView.View = System.Windows.Forms.View.Details;
-            this.SummaryListView.Resize += new System.EventHandler(this.SummaryListView_Resize);
+            SummaryListView.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            SummaryListView.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2 });
+            SummaryListView.FullRowSelect = true;
+            SummaryListView.HeaderStyle = ColumnHeaderStyle.None;
+            SummaryListView.Location = new Point(9, 31);
+            SummaryListView.Margin = new Padding(0);
+            SummaryListView.Name = "SummaryListView";
+            SummaryListView.Size = new Size(204, 400);
+            SummaryListView.TabIndex = 13;
+            SummaryListView.UseCompatibleStateImageBehavior = false;
+            SummaryListView.View = View.Details;
+            SummaryListView.Resize += SummaryListView_Resize;
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "Key";
-            this.columnHeader1.Width = 90;
+            columnHeader1.Text = "Key";
+            columnHeader1.Width = 90;
             // 
             // columnHeader2
             // 
-            this.columnHeader2.Text = "Value";
-            this.columnHeader2.Width = 110;
+            columnHeader2.Text = "Value";
+            columnHeader2.Width = 110;
             // 
             // SummaryLabel
             // 
-            this.SummaryLabel.AutoSize = true;
-            this.SummaryLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.SummaryLabel.Location = new System.Drawing.Point(9, 10);
-            this.SummaryLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 5);
-            this.SummaryLabel.Name = "SummaryLabel";
-            this.SummaryLabel.Size = new System.Drawing.Size(67, 17);
-            this.SummaryLabel.TabIndex = 11;
-            this.SummaryLabel.Text = "Summary";
+            SummaryLabel.AutoSize = true;
+            SummaryLabel.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            SummaryLabel.Location = new Point(9, 10);
+            SummaryLabel.Margin = new Padding(0, 0, 0, 5);
+            SummaryLabel.Name = "SummaryLabel";
+            SummaryLabel.Size = new Size(67, 17);
+            SummaryLabel.TabIndex = 11;
+            SummaryLabel.Text = "Summary";
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(0);
-            this.splitContainer1.Name = "splitContainer1";
+            splitContainer1.Dock = DockStyle.Top;
+            splitContainer1.FixedPanel = FixedPanel.Panel2;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Margin = new Padding(0);
+            splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.SummaryListView);
-            this.splitContainer1.Panel1.Controls.Add(this.SummaryLabel);
-            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(10);
+            splitContainer1.Panel1.Controls.Add(SummaryListView);
+            splitContainer1.Panel1.Controls.Add(SummaryLabel);
+            splitContainer1.Panel1.Padding = new Padding(10);
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.LocationsTreeView);
-            this.splitContainer1.Panel2.Controls.Add(this.LocationsLabel);
-            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(10);
-            this.splitContainer1.Size = new System.Drawing.Size(478, 566);
-            this.splitContainer1.SplitterDistance = 222;
-            this.splitContainer1.TabIndex = 14;
+            splitContainer1.Panel2.Controls.Add(LocationsTreeView);
+            splitContainer1.Panel2.Controls.Add(LocationsLabel);
+            splitContainer1.Panel2.Padding = new Padding(10);
+            splitContainer1.Size = new Size(478, 566);
+            splitContainer1.SplitterDistance = 222;
+            splitContainer1.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 565);
-            this.Controls.Add(this.splitContainer1);
-            this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(800, 604);
-            this.MinimumSize = new System.Drawing.Size(494, 604);
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "This PC";
-            this.Activated += new System.EventHandler(this.Form1_Activated);
-            this.Click += new System.EventHandler(this.Form1_Click);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 565);
+            Controls.Add(splitContainer1);
+            MaximizeBox = false;
+            MaximumSize = new Size(800, 604);
+            MinimumSize = new Size(494, 604);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "This PC";
+            Activated += Form1_Activated;
+            Click += Form1_Click;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
